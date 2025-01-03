a3e15e0a663bbb7b7d2ee283afe381a1b600f0fe
jinjiabao.jjb <jinjiabao.jjb@antgroup.com>
2025-01-03 15:51:53 +0800

    modify
    Signed-off-by: jinjiabao.jjb <jinjiabao.jjb@antgroup.com>

--- tests/test_hnsw_new.cpp
+++ tests/test_hnsw_new.cpp
@@ -275,11 +275,11 @@ TEST_CASE_PERSISTENT_FIXTURE(fixtures::HNSWTestIndex, "HNSW Concurrent Add", "[f
 
         auto dataset = pool.GetDatasetAndCreate(dim, base_count, metric_type);
         TestConcurrentAdd(index, dataset, true);
-        TestKnnSearch(index, dataset, search_param, 0.98, true);
-        TestConcurrentKnnSearch(index, dataset, search_param, 0.98, true);
-        TestRangeSearch(index, dataset, search_param, 0.98, 10, true);
-        TestRangeSearch(index, dataset, search_param, 0.48, 5, true);
-        TestFilterSearch(index, dataset, search_param, 0.98, true);
+        TestKnnSearch(index, dataset, search_param, 0.95, true);
+        TestConcurrentKnnSearch(index, dataset, search_param, 0.95, true);
+        TestRangeSearch(index, dataset, search_param, 0.95, 10, true);
+        TestRangeSearch(index, dataset, search_param, 0.45, 5, true);
+        TestFilterSearch(index, dataset, search_param, 0.95, true);
 
         vsag::Options::Instance().set_block_size_limit(origin_size);
     }
