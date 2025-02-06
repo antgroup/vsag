95327b6152154b8f44f93b9bfcef3c99d3618718
Roxanne0321 <liruoxvan@qq.com>
2025-02-06 16:29:48 +0800

    fifth

--- src/quantization/quantizer.h
+++ src/quantization/quantizer.h
@@ -141,7 +141,7 @@ public:
         StreamReader::ReadObj(reader, this->dim_);
         StreamReader::ReadObj(reader, this->metric_);
         StreamReader::ReadObj(reader, this->code_size_);
-        StreamWriter::ReadObj(writer, this->is_trained_);
+        StreamReader::ReadObj(writer, this->is_trained_);
         return cast().DeserializeImpl(reader);
     }
 
