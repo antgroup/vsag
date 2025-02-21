aaa0f2cf57c474a3a401d5d2d189021ccfe69a38
jinjiabao.jjb <jinjiabao.jjb@antgroup.com>
2025-02-21 17:30:30 +0800

    modify
    Signed-off-by: jinjiabao.jjb <jinjiabao.jjb@antgroup.com>

--- src/data_cell/graph_datacell.h
+++ src/data_cell/graph_datacell.h
@@ -118,8 +118,8 @@ GraphDataCell<IOTmpl, false>::InsertNeighborsById(InnerIdType id,
         logger::warn(fmt::format(
             "insert neighbors count {} more than {}", neighbor_ids.size(), this->maximum_degree_));
     }
-    total_count_ = std::max(total_count_, id + 1);
-    max_capacity_ = std::max(total_count_, max_capacity_);
+    total_count_ = std::max(total_count_.load(), id + 1);
+    max_capacity_ = std::max(total_count_.load(), max_capacity_);
     auto start = static_cast<uint64_t>(id) * static_cast<uint64_t>(this->code_line_size_);
     uint32_t neighbor_count = std::min((uint32_t)(neighbor_ids.size()), this->maximum_degree_);
     this->io_->Write((uint8_t*)(&neighbor_count), sizeof(neighbor_count), start);
