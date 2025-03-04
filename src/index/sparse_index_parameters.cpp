440888dccf5e285c82246488be86466a0316637d
jinjiabao.jjb <jinjiabao.jjb@antgroup.com>
2025-03-04 20:21:10 +0800

    modify
    Signed-off-by: jinjiabao.jjb <jinjiabao.jjb@antgroup.com>

---
+++ src/index/sparse_index_parameters.cpp
@@ -0,0 +1,5 @@
+//
+// Created by root on 3/4/25.
+//
+
+#include "sparse_index_parameters.h"
---
+++ src/index/sparse_index_parameters.h
@@ -0,0 +1,16 @@
+//
+// Created by root on 3/4/25.
+//
+
+#ifndef VSAG_SPARSE_INDEX_PARAMETERS_H
+#define VSAG_SPARSE_INDEX_PARAMETERS_H
+
+
+
+class sparse_index_parameters {
+
+};
+
+
+
+#endif //VSAG_SPARSE_INDEX_PARAMETERS_H
