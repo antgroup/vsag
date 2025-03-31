11207ba5d277844175bae7a828cb7e5f60b7e428
root <root@sqa011124122093.eu126>
2025-03-31 15:58:24 +0800

    support gno-imi partition
    Signed-off-by: suguan.dx <suguan.dx@antgroup.con>

--- src/impl/kmeans_cluster.h
+++ src/impl/kmeans_cluster.h
@@ -30,7 +30,7 @@ public:
     ~KMeansCluster();
 
     Vector<int>
-    Run(uint32_t k, const float* datas, uint64_t count, int iter = 25);
+    Run(uint32_t k, const float* datas, uint64_t count, int iter = 25, double* err = nullptr);
 
 public:
     float* k_centroids_{nullptr};
@@ -42,7 +42,8 @@ private:
                                const uint64_t k,
                                float* y_sqr,
                                float* distances,
-                               Vector<int32_t>& labels);
+                               Vector<int32_t>& labels,
+                               float* errs);
 
     double
     find_nearest_one_with_hgraph(const float* query,
