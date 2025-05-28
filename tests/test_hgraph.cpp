f9537f2d8b20ce0018840541c5a1985a1c9f26b5
Deming Chu <chudeming.cdm@antgroup.com>
2025-05-28 14:34:03 +0800

    test(hgraph): GetMemoryUsageDetail, improve cov
    Signed-off-by: Deming Chu <chudeming.cdm@antgroup.com>

--- tests/test_hgraph.cpp
+++ tests/test_hgraph.cpp
@@ -528,6 +528,38 @@ TEST_CASE_PERSISTENT_FIXTURE(fixtures::HgraphTestIndex,
     }
 }
 
+TEST_CASE_PERSISTENT_FIXTURE(fixtures::HgraphTestIndex,
+                             "HGraph GetMemoryUsageDetail",
+                             "[ft][hgraph]") {
+    auto origin_size = vsag::Options::Instance().block_size_limit();
+    auto size = GENERATE(1024 * 1024 * 2);
+    auto metric_type = GENERATE("l2", "ip", "cosine");
+
+    const std::string name = "hgraph";
+    auto search_param = fmt::format(search_param_tmp, 200, false);
+    for (auto dim : dims) {
+        for (auto& [base_quantization_str, recall] : test_cases) {
+            if (IsRaBitQ(base_quantization_str)) {
+                if (std::string(metric_type) != "l2") {
+                    continue;
+                }
+                if (dim <= fixtures::RABITQ_MIN_RACALL_DIM) {
+                    dim += fixtures::RABITQ_MIN_RACALL_DIM;
+                }
+            }
+            vsag::Options::Instance().set_block_size_limit(size);
+            auto param =
+                GenerateHGraphBuildParametersString(metric_type, dim, base_quantization_str);
+            auto index = TestFactory(name, param, true);
+            auto memory_detail = index->GetMemoryUsageDetail();
+            REQUIRE(memory_detail.contains("basic_flatten_codes"));
+            REQUIRE(memory_detail.contains("bottom_graph"));
+            REQUIRE(memory_detail.contains("route_graph"));
+            vsag::Options::Instance().set_block_size_limit(origin_size);
+        }
+    }
+}
+
 TEST_CASE_PERSISTENT_FIXTURE(fixtures::HgraphTestIndex, "HGraph Add", "[ft][hgraph]") {
     auto origin_size = vsag::Options::Instance().block_size_limit();
     auto size = GENERATE(1024 * 1024 * 2);
