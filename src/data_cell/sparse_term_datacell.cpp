b9b3e1f82e499b4324365de82e0f11859e68c3f5
zhongxiaoyao.zxy <zhongxiaoyao.zxy@antgroup.com>
2025-07-30 17:26:29 +0800

    update
    Signed-off-by: zhongxiaoyao.zxy <zhongxiaoyao.zxy@antgroup.com>

--- src/data_cell/sparse_term_datacell.cpp
+++ src/data_cell/sparse_term_datacell.cpp
@@ -72,8 +72,8 @@ SparseTermDataCell::InsertHeap(float* dists,
         }
 
         uint32_t i = 0;
-        uint32_t term_size = static_cast<uint32_t>(static_cast<float>(term_sizes_[term]) *
-                                                   computer->term_prune_ratio_);
+        auto term_size = static_cast<uint32_t>(static_cast<float>(term_sizes_[term]) *
+                                               computer->term_prune_ratio_);
         bool is_valid = false;
         if constexpr (mode == InnerSearchMode::KNN_SEARCH) {
             if (heap.size() < n_candidate) {
