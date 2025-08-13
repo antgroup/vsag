842a3651228cc8b601fb215b7d7923e30e935230
jinjiabao.jjb <jinjiabao.jjb@antgroup.com>
2025-08-13 16:09:40 +0800

    modify
    Signed-off-by: jinjiabao.jjb <jinjiabao.jjb@antgroup.com>

--- extern/tsl/tsl.cmake
+++ extern/tsl/tsl.cmake
@@ -4,8 +4,8 @@ include(FetchContent)
 FetchContent_Declare(
         tsl
         URL https://github.com/Tessil/robin-map/archive/refs/tags/v1.4.0.tar.gz
-        # this url is maintained by the vsag project, if it's broken, please try
-        #  the latest commit or contact the vsag project
+            https://vsagcache.oss-rg-china-mainland.aliyuncs.com/robin-map/v1.4.0.tar.gz
+        URL_HASH MD5=d56a879c94e021c55d8956e37deb3e4f
 
         DOWNLOAD_NO_PROGRESS 1
         INACTIVITY_TIMEOUT 5
