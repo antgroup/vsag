42a3aa100bee594d2ee5835bdd0738345bca248f
jinjiabao.jjb <jinjiabao.jjb@antgroup.com>
2025-09-08 21:06:06 +0800

    modify
    Signed-off-by: jinjiabao.jjb <jinjiabao.jjb@antgroup.com>

--- extern/cpr/cpr.cmake
+++ extern/cpr/cpr.cmake
@@ -13,23 +13,6 @@ FetchContent_GetProperties(cpr)
 if (NOT cpr_POPULATED)
     FetchContent_Populate(cpr)
 
-    # check patch marker
-    if (NOT EXISTS "${cpr_SOURCE_DIR}/patch_applied_marker")
-        set(PATCH_FILE "${CMAKE_SOURCE_DIR}/extern/cpr/fix_curl.patch")
-        execute_process(
-                COMMAND patch -p1
-                INPUT_FILE ${PATCH_FILE}
-                WORKING_DIRECTORY ${cpr_SOURCE_DIR}
-                RESULT_VARIABLE PATCH_RESULT
-        )
-        if (NOT PATCH_RESULT EQUAL 0)
-            message(FATAL_ERROR "Failed to apply patch CURL, please make clean and retry.")
-        endif ()
-
-        # create a marker
-        file(WRITE "${cpr_SOURCE_DIR}/patch_applied_marker" "Patch applied")
-        message(STATUE "Write patch marker in ${cpr_SOURCE_DIR}/patch_applied_marker")
-    endif ()
 endif ()
 
 set(CPR_ENABLE_SSL OFF)
