7933b028cd94eec4c211732571924ea81c0a7d72
Coien-rr <972054003@qq.com>
2025-09-28 23:48:07 +0800

    wip(quantization): remove friend class definition
    Signed-off-by: Coien-rr <972054003@qq.com>

--- src/quantization/quantizer.h
+++ src/quantization/quantizer.h
@@ -19,16 +19,12 @@
 #include <memory>
 
 #include "computer.h"
-#include "impl/logger/logger.h"
 #include "metric_type.h"
 #include "storage/stream_reader.h"
 #include "storage/stream_writer.h"
 #include "utils/function_exists_check.h"
 
 namespace vsag {
-
-template <typename Q, typename D>
-class QuantizerAdapter;
 
 using DataType = float;
 
@@ -263,9 +259,6 @@ private:
     }
 
     friend QuantT;
-
-    template <typename Q, typename D>
-    friend class QuantizerAdapter;
 
 private:
     uint64_t dim_{0};
