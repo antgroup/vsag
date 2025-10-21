ad331d9421c332ece9e505088bb1784da37eebc0
zhongxiaoyao.zxy <zhongxiaoyao.zxy@antgroup.com>
2025-10-21 16:06:40 +0800

    improve code
    Signed-off-by: zhongxiaoyao.zxy <zhongxiaoyao.zxy@antgroup.com>

--- src/quantization/transform_quantization/transform_quantizer.h
+++ src/quantization/transform_quantization/transform_quantizer.h
@@ -193,6 +193,8 @@ TransformQuantizer<QuantTmpl, metric>::MakeTransformerInstance(
 template <typename QuantTmpl, MetricType metric>
 bool
 TransformQuantizer<QuantTmpl, metric>::TrainImpl(const DataType* data, uint64_t count) {
+    count = std::min(count, (uint64_t)TQ_MAX_TRAIN_COUNT);
+
     // 1. train transformer based on original data
     for (const auto& vector_transformer : this->transform_chain_) {
         vector_transformer->Train(data, count);
