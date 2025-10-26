abe4b91addd64c750feda5abd25e95edbc965b89
misaka0714 <129934985+misaka0714@users.noreply.github.com>
2025-10-26 23:03:52 +0800

    Update train sample size handling in PQFastScanQuantizer
    Signed-off-by: misaka0714 <129934985+misaka0714@users.noreply.github.com>

--- src/quantization/product_quantization/pq_fastscan_quantizer.cpp
+++ src/quantization/product_quantization/pq_fastscan_quantizer.cpp
@@ -52,6 +52,7 @@ PQFastScanQuantizer<metric>::PQFastScanQuantizer(const PQFastScanQuantizerParamP
                                                  const IndexCommonParam& common_param)
     : PQFastScanQuantizer<metric>(
           common_param.dim_, param->pq_dim_, common_param.allocator_.get()) {
+    this->train_sample_size_ = param->train_sample_size_;
 }
 
 template <MetricType metric>
@@ -67,7 +68,7 @@ PQFastScanQuantizer<metric>::TrainImpl(const vsag::DataType* data, uint64_t coun
     if (this->is_trained_) {
         return true;
     }
-    count = std::min(count, 65536UL);
+    count = std::min(count, static_cast<uint64_t> (this->train_sample_size_));
     Vector<float> slice(this->allocator_);
     slice.resize(count * subspace_dim_);
     Vector<float> norm_data(this->allocator_);
