fe3246b10539abc886789884218dcb8b061c24a7
misaka0714 <129934985+misaka0714@users.noreply.github.com>
2025-10-28 08:35:40 +0800

    Update pq_fastscan_quantizer_parameter_test.cpp
    Signed-off-by: misaka0714 <129934985+misaka0714@users.noreply.github.com>

--- src/quantization/product_quantization/pq_fastscan_quantizer_parameter_test.cpp
+++ src/quantization/product_quantization/pq_fastscan_quantizer_parameter_test.cpp
@@ -24,15 +24,13 @@ using namespace vsag;
 TEST_CASE("PQFS Parameter ToJson Test", "[ut][PQFastScanQuantizerParameter]") {
     std::string param_str = R"(
         {
-            "pq_dim": 64,
-            "pq_train_sample_size": 65535
+            "pq_dim": 64
         }
     )";
     auto param = std::make_shared<PQFastScanQuantizerParameter>();
     param->FromJson(JsonType::Parse(param_str));
     ParameterTest::TestToJson(param);
     REQUIRE(param->pq_dim_ == 64);
-    REQUIRE(param->train_sample_size_ == 65535);
 
     TestParamCheckCompatibility<PQFastScanQuantizerParameter>(param_str);
 }
