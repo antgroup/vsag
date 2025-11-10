7068a2e5f906c2bf985777de9bc639410ec807c9
liruoxuan.lrx <liruoxuan.lrx@antgroup.com>
2025-11-10 11:41:50 +0800

    feat(sparse): revise doc prune function
    Signed-off-by: liruoxuan.lrx <liruoxuan.lrx@antgroup.com>

--- src/datacell/sparse_term_datacell.cpp
+++ src/datacell/sparse_term_datacell.cpp
@@ -151,7 +151,7 @@ SparseTermDataCell::DocPrune(Vector<std::pair<uint32_t, float>>& sorted_base) co
     float temp_mass = 0.0f;
     int pruned_doc_len = 0;
 
-    while(temp_mass < part_mass) {
+    while (temp_mass < part_mass) {
         temp_mass += sorted_base[pruned_doc_len++].second;
     }
 
