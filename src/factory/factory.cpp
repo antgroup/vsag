24e9684dda1ca1104025d7e4ba2c7f43e8dd7d10
jinjiabao.jjb <jinjiabao.jjb@antgroup.com>
2025-12-09 16:39:02 +0800

    fix(LocalFileReader): fix thread safety issues in LocalFileReader
    Signed-off-by: jinjiabao.jjb <jinjiabao.jjb@antgroup.com>

--- src/factory/factory.cpp
+++ src/factory/factory.cpp
@@ -70,8 +70,11 @@ public:
 
     void
     AsyncRead(uint64_t offset, uint64_t len, void* dest, CallBack callback) override {
-        if (not pool_) {
-            pool_ = SafeThreadPool::FactoryDefaultThreadPool();
+        {
+            std::lock_guard<std::mutex> lock(mutex_);
+            if (not pool_) {
+                pool_ = SafeThreadPool::FactoryDefaultThreadPool();
+            }
         }
         pool_->GeneralEnqueue([this,  // NOLINT(clang-analyzer-cplusplus.NewDeleteLeaks)
                                offset,
