ddaf64bec3a354bf6e7e2b1910e986635c2b9e2b
xin ning <xinning@U-GJ4YX14D-0038.local>
2025-12-26 12:03:34 +0800

    refactor sparse index sindi
    Signed-off-by: xin ning <xinning@U-GJ4YX14D-0038.local>

--- src/datacell/sparse_term_datacell.h
+++ src/datacell/sparse_term_datacell.h
@@ -99,6 +99,28 @@ public:
     void
     GetSparseVector(uint32_t base_id, SparseVector* data, Allocator* specified_allocator);
 
+private:
+    template <InnerSearchMode mode, InnerSearchType type>
+    void
+    InsertCandidateIntoHeap(uint32_t id,
+                            float* dists,
+                            float& cur_heap_top,
+                            MaxHeap& heap,
+                            uint32_t offset_id,
+                            uint32_t n_candidate,
+                            float radius,
+                            const FilterPtr& filter) const;
+
+    template <InnerSearchType type>
+    bool
+    FillHeapInitial(uint32_t id,
+                    float* dists,
+                    float& cur_heap_top,
+                    MaxHeap& heap,
+                    uint32_t offset_id,
+                    uint32_t n_candidate,
+                    const FilterPtr& filter) const;
+
 public:
     uint32_t term_id_limit_{0};
 
