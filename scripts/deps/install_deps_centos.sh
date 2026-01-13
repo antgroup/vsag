6d23e28284705904374425175421fb1979ae8830
LHT129 <tianlan.lht@antgroup.com>
2026-01-13 13:43:36 +0800

    feat(io_uring): add new IO Type: UringIO
    Signed-off-by: LHT129 <tianlan.lht@antgroup.com>

--- scripts/deps/install_deps_centos.sh
+++ scripts/deps/install_deps_centos.sh
@@ -1,18 +1,11 @@
 # install python3-devel
-yum install -y gcc gcc-c++ gcc-gfortran python3-devel libaio-devel libcurl-devel
+yum install -y gfortran python3-devel libaio-devel libcurl-devel liburing-devel
 
-ARCH=$(uname -m)
+# install openmp for alibaba clang 11
+yum install -y current -y libomp11-devel libomp11
 
-if [[ "${ARCH}" == "x86_64" ]]; then
-    # install openmp for alibaba clang 11 (only available on x86_64)
-    yum install -b current -y libomp11-devel libomp11
-
-    # install Intel MKL (only available on x86_64)
-    yum install -y ca-certificates
-    yum-config-manager --add-repo https://yum.repos.intel.com/mkl/setup/intel-mkl.repo
-    rpm --import https://yum.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB
-    yum install -y intel-mkl-64bit-2020.0-088
-else
-    # MKL is not supported on aarch64. We fallback to openblas/generic openmp.
-    yum install -y ca-certificates libgomp
-fi
+# install Intel MKL
+yum install -y ca-certificates
+yum-config-manager --add-repo https://yum.repos.intel.com/mkl/setup/intel-mkl.repo
+rpm --import https://yum.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB
+yum install -y intel-mkl-64bit-2020.0-088
