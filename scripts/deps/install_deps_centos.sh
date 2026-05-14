a13b6c2f7cb89276e88dea73fabbc5ad5a781dc1
Xiangyu Wang <wxy407827@antgroup.com>
2026-05-14 18:45:48 +0800

    fix: keep CentOS Clang OpenMP dependency
    Signed-off-by: Xiangyu Wang <wxy407827@antgroup.com>

--- scripts/deps/install_deps_centos.sh
+++ scripts/deps/install_deps_centos.sh
@@ -1,4 +1,7 @@
 # install python3-devel
 yum install -y gfortran python3-devel libaio-devel libcurl-devel ca-certificates
 
+# install openmp for alibaba clang 11
+yum install -b current -y libomp11-devel libomp11
+
 yum install -y libgomp
