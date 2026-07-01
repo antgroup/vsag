bf8f148258ded4ddbee220bd6ce60bedc5867f18
jc543239 <jc543239@antgroup.com>
2026-07-01 03:32:19 +0000

    optimize
    Signed-off-by: jc543239 <jc543239@antgroup.com>

--- src/algorithm/hgraph/hgraph_build.cpp
+++ src/algorithm/hgraph/hgraph_build.cpp
@@ -644,7 +644,7 @@ HGraph::ensure_physical_code_capacity_unlocked(InnerIdType required_capacity) {
     }
 
     auto new_capacity = static_cast<InnerIdType>(
-        next_multiple_of_power_of_two(required_capacity, DEFAULT_RESIZE_BIT));
+        next_multiple_of_power_of_two(required_capacity, this->resize_increase_count_bit_));
     this->basic_flatten_codes_->Resize(new_capacity);
     if (has_precise_reorder()) {
         this->high_precise_codes_->Resize(new_capacity);
